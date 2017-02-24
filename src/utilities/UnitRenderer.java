38efee623e9f1eba74cbb9a04851c8a53ec23851
Jonathen Settle <jonathenksettle@ufl.edu>
2017-02-24 11:16:41 -0500

    Created renderers for units and structures.

---
+++ src/utilities/UnitRenderer.java
@@ -0,0 +1,33 @@
+package utilities;
+
+import model.entities.unit.*;
+
+/**
+ * Created by Jonathen on 2/24/2017.
+ */
+public class UnitRenderer {
+
+    void render(final Army army) {
+
+    }
+
+    void render(final Colonist colonist) {
+
+    }
+
+    void render(final Explorer explorer) {
+
+    }
+
+    void render(final Melee melee) {
+
+    }
+
+    void render(final Ranged ranged) {
+
+    }
+
+    void render(final Worker worker) {
+
+    }
+}
