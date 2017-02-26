acdc2db4d701bdceb896f9eab3ebc06b8a5b2993
cduica <crisd0000@gmail.com>
2017-02-26 17:44:54 -0500

    added map stuff

--- src/model/map/Map.java
+++ src/model/map/Map.java
@@ -2,11 +2,20 @@ package model.map;
 
 import model.common.Location;
 import model.map.tile.Tile;
+import model.map.tile.areaeffects.AreaEffect;
+import model.map.tile.areaeffects.AreaEffectFactory;
+import model.map.tile.areaeffects.AreaEffectType;
+import model.map.tile.areaeffects.DecalType;
+import model.map.tile.resources.Resource;
+import model.map.tile.resources.ResourceType;
+import model.map.tile.terrain.Terrain;
+import model.map.tile.terrain.TerrainType;
+import utilities.MapXMLParser;
 import utilities.Observer;
 import utilities.Subject;
 
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 
 /**
  * Created by cduica on 2/21/17.
@@ -23,19 +32,78 @@ public class Map implements Subject {
     private final String XML_PATH = "res/map/Map.xml";
 
     public Map() {
+        tiles = new HashMap<>();
         init();
     }
 
     private void init(){
 
+        MapXMLParser mapXMLParser = new MapXMLParser();
+
+        try {
+            mapXMLParser.loadDocument(XML_PATH);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        ArrayList<HashMap<String, String>> tileList = mapXMLParser.parseDocument();
+
+        AreaEffectFactory areaEffectFactory = new AreaEffectFactory();
+
+        int x = 0;
+        int y = 0;
+
+        for(int i = 0; i < tileList.size(); i++){
+
+            HashMap<String , String > map = tileList.get(i);
+            ResourceType resourceType= ResourceType.EMPTY;
+            DecalType decalType= DecalType.EMPTY;
+            TerrainType terrainType= TerrainType.EMPTY;
+
+            if (!map.get("Resource").equals("")) {
+                resourceType = ResourceType.valueOf(map.get("Resource"));
+            }
+            if (!map.get("Terrain").equals("")) {
+                terrainType = TerrainType.valueOf(map.get("Terrain"));
+            }
+            if (!map.get("Decal").equals("")) {
+                decalType = DecalType.valueOf(map.get("Decal"));
+            }
+
+            AreaEffect areaEffect = areaEffectFactory.createAreaEffect(AreaEffectType.valueOf(map.get("AreaEffect")));
+
+            Tile tile = new Tile(
+                    new Terrain(terrainType, decalType),
+                    areaEffect,
+                    new Resource(resourceType),
+                    null
+            );
+
+            if(i%width==0){
+                y++;
+            }
+
+            if(y%width == 0){
+                x = 0;
+            } else {
+                x++;
+            }
+
+            Location location = new Location(x, y);
+
+            tiles.put(location, tile);
+
+        }
+
     }
 
-    public Tile getTile(Location point){
+    public HashMap<Location, Tile> getAdjacentTiles(Location location){
+
         return null;
     }
 
-    public List<Tile> getAdjacentTiles(Location point){
-        return null;
+    public Tile getTile(Location location){
+        return tiles.get(location);
     }
 
     @Override
---
+++ src/utilities/MapXMLParser.java
@@ -0,0 +1,54 @@
+package utilities;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created by cduica on 2/26/17.
+ */
+public class MapXMLParser {
+
+    private Document doc;
+
+    public void loadDocument(String path) throws Exception {
+        File xmlFile = new File(path);
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+        doc = dBuilder.parse(xmlFile);
+        doc.getDocumentElement().normalize();
+    }
+
+    public ArrayList<HashMap<String, String>> parseDocument(){
+        ArrayList<HashMap<String, String>> tileList = new ArrayList<>();
+
+        NodeList nodeList = doc.getElementsByTagName("Tile");
+        for(int i = 0; i < nodeList.getLength(); i++){
+            Node node = nodeList.item(i);
+            if(node.getNodeType() == Node.ELEMENT_NODE){
+                Element element = (Element) node;
+                HashMap<String, String> temp = new HashMap<>();
+                temp.put("Terrain", element.getElementsByTagName("Terrain").item(0).getTextContent());
+                temp.put("Resource", element.getElementsByTagName("Resource").item(0).getTextContent());
+                temp.put("Decal", element.getElementsByTagName("Decal").item(0).getTextContent());
+                temp.put("Item", element.getElementsByTagName("Item").item(0).getTextContent());
+                temp.put("AreaEffect", element.getElementsByTagName("AreaEffect").item(0).getTextContent());
+                tileList.add(temp);
+            }
+        }
+        return tileList;
+    }
+
+    public String getMapAttribute(String attrName){
+        Node node = doc.getElementsByTagName("Map").item(0);
+        return  ((Element) node).getAttribute(attrName);
+    }
+
+}
