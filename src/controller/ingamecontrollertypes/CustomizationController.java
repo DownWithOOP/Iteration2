93f6f564950f7017b9726f208006a8cf1b13f04e
konradpabjan <konradpabjan@outlook.com>
2017-03-15 09:29:14 -0400

    Configuration Import

--- src/controller/ingamecontrollertypes/CustomizationController.java
+++ src/controller/ingamecontrollertypes/CustomizationController.java
@@ -6,12 +6,14 @@ import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.stage.FileChooser;
 import view.utilities.FileWriter;
-
+import java.io.File;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.ResourceBundle;
+import java.util.Scanner;
 
 /**
  * Created by Konrad on 3/15/2017.
@@ -19,6 +21,7 @@ import java.util.ResourceBundle;
 public class CustomizationController extends Controller {
 
     private HashMap<KeyCode,String> playerHashMap;
+    private HashMap<String,KeyCode> reference;
     private FileWriter fileWriter;
 
 
@@ -65,6 +68,45 @@ public class CustomizationController extends Controller {
                 keyFive.setText(pair.getKey().toString());
             } else if(pair.getValue().equals("NE")){
                 keySix.setText(pair.getKey().toString());
+            }
+        }
+    }
+
+    @FXML
+    private void importData(){
+        final FileChooser fileChooser = new FileChooser();
+        File file = fileChooser.showOpenDialog(this.keyFive.getScene().getWindow());
+        if (file != null) {
+            try{
+                playerHashMap.clear();
+                Scanner input = new Scanner(file);
+                while(input.hasNextLine()){
+                    String line1 = input.nextLine();
+                    String line2 = input.nextLine();
+                    KeyCode temp = (KeyCode)reference.get(line2);
+                    playerHashMap.put(temp, line1);
+
+                    if(line1.equals("NORTH")){
+                        keyOne.setText(" : " + temp);
+                    }
+                    if(line1.equals("SOUTH")){
+                        keyTwo.setText(" : " + temp);
+                    }
+                    if(line1.equals("NE")){
+                        keyThree.setText(" : " + temp);
+                    }
+                    if(line1.equals("SE")){
+                        keyFour.setText(" : " + temp);
+                    }
+                    if(line1.equals("NW")){
+                        keyFive.setText(" : " + temp);
+                    }
+                    if(line1.equals("SW")){
+                        keySix.setText(" : " + temp);
+                    }
+                }
+            } catch(Exception e){
+                e.printStackTrace();
             }
         }
     }
@@ -120,6 +162,98 @@ public class CustomizationController extends Controller {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         this.fileWriter = new FileWriter();
+
+        // initialize map
+        this.reference = new HashMap<>();
+
+        // numpad
+        reference.put("NUMPAD1", KeyCode.NUMPAD1);
+        reference.put("NUMPAD2", KeyCode.NUMPAD2);
+        reference.put("NUMPAD3", KeyCode.NUMPAD3);
+        reference.put("NUMPAD4", KeyCode.NUMPAD4);
+        reference.put("NUMPAD5", KeyCode.NUMPAD5);
+        reference.put("NUMPAD6", KeyCode.NUMPAD6);
+        reference.put("NUMPAD7", KeyCode.NUMPAD7);
+        reference.put("NUMPAD8",KeyCode.NUMPAD8);
+        reference.put("NUMPAD9",KeyCode.NUMPAD9);
+
+        // digits
+        reference.put("DIGIT0",KeyCode.DIGIT0);
+        reference.put("DIGIT1",KeyCode.DIGIT1);
+        reference.put("DIGIT2",KeyCode.DIGIT2);
+        reference.put("DIGIT3",KeyCode.DIGIT3);
+        reference.put("DIGIT4",KeyCode.DIGIT4);
+        reference.put("DIGIT5",KeyCode.DIGIT5);
+        reference.put("DIGIT6",KeyCode.DIGIT6);
+        reference.put("DIGIT7",KeyCode.DIGIT7);
+        reference.put("DIGIT8",KeyCode.DIGIT8);
+        reference.put("DIGIT9",KeyCode.DIGIT9);
+
+        // all numbers
+        reference.put("a",KeyCode.A);
+        reference.put("A",KeyCode.A);
+        reference.put("b",KeyCode.B);
+        reference.put("B",KeyCode.B);
+        reference.put("c",KeyCode.C);
+        reference.put("C",KeyCode.C);
+        reference.put("d",KeyCode.D);
+        reference.put("D",KeyCode.D);
+        reference.put("e",KeyCode.E);
+        reference.put("E",KeyCode.E);
+        reference.put("f",KeyCode.F);
+        reference.put("F",KeyCode.F);
+        reference.put("g",KeyCode.G);
+        reference.put("G",KeyCode.G);
+        reference.put("h",KeyCode.H);
+        reference.put("H",KeyCode.H);
+        reference.put("I",KeyCode.I);
+        reference.put("i",KeyCode.I);
+        reference.put("J",KeyCode.J);
+        reference.put("j",KeyCode.J);
+        reference.put("K",KeyCode.K);
+        reference.put("k",KeyCode.K);
+        reference.put("l",KeyCode.L);
+        reference.put("L",KeyCode.L);
+        reference.put("M",KeyCode.M);
+        reference.put("m",KeyCode.M);
+        reference.put("n",KeyCode.N);
+        reference.put("N",KeyCode.N);
+        reference.put("o",KeyCode.O);
+        reference.put("O",KeyCode.O);
+        reference.put("p",KeyCode.P);
+        reference.put("P",KeyCode.P);
+        reference.put("Q",KeyCode.Q);
+        reference.put("q",KeyCode.Q);
+        reference.put("R",KeyCode.R);
+        reference.put("r",KeyCode.R);
+        reference.put("S",KeyCode.S);
+        reference.put("s",KeyCode.S);
+        reference.put("T",KeyCode.T);
+        reference.put("t",KeyCode.T);
+        reference.put("U",KeyCode.U);
+        reference.put("u",KeyCode.U);
+        reference.put("V",KeyCode.V);
+        reference.put("v",KeyCode.V);
+        reference.put("W",KeyCode.W);
+        reference.put("w",KeyCode.W);
+        reference.put("X",KeyCode.X);
+        reference.put("x",KeyCode.X);
+        reference.put("Y",KeyCode.Y);
+        reference.put("y",KeyCode.Y);
+        reference.put("Z",KeyCode.Z);
+        reference.put("z",KeyCode.Z);
+
+        //
+        reference.put("SPACE",KeyCode.SPACE);
+        reference.put("BACK_SPACE",KeyCode.BACK_SPACE);
+        reference.put("ENTER",KeyCode.ENTER);
+        reference.put("SHIFT",KeyCode.SHIFT);
+        reference.put("UP",KeyCode.UP);
+        reference.put("DOWN",KeyCode.DOWN);
+        reference.put("LEFT",KeyCode.LEFT);
+        reference.put("RIGHT",KeyCode.RIGHT);
+
+
 
         this.keyOne.addEventFilter(KeyEvent.KEY_PRESSED, event ->
         {
